673e709ddded9fa3b804852a6b880dbfddb96d9f
Sampoorna Suthakar <sampooit12@gmail.com>
2022-08-13 23:42:08 +0530

    final clean up

--- setupJest.js
+++
@@ -1,9 +0,0 @@
-require("jest-fetch-mock").enableMocks();
-const obj = require("@svgdotjs/svg.js");
-const SVG = (arg) => {
-  return obj.SVG(arg);
-};
-
-Object.assign(SVG, obj);
-
-global.SVG = SVG;
